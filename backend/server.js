67840fb27aafaa03f8cb614c04955c567e8be019
karthikeyan-ss <karthikeyanseela@gmail.com>
2025-02-23 17:30:41 +0530

    Cors issues fixed

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,24 @@ const restaurantRoutes = require('./routes/restaurantRoutes');
 
 dotenv.config();
 
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://zomato-restaurant-listing-n-searching.vercel.app", // deployed frontend
+];
+
 const app = express();
 
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use('/restaurants', restaurantRoutes);
 
 
